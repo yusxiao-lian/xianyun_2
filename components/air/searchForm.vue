dffb6d2f9a5e3393cfed07d8d91534015ec295d7
lianyuxiao <1049146413@qq.com>
2020-02-13 01:11:08 +0800

    完成机票首页

---
+++ components/air/searchForm.vue
@@ -0,0 +1,301 @@
+<template>
+    <div class="search-form">
+
+        <!-- 头部tab切换 -->
+        <el-row type="flex" class="search-tab">
+            <span v-for="(item, index) in tabs" :key="index"
+            @click="handleSearchTab(item, index)"
+            :class="{active: index === currentTab}">
+                <i :class="item.icon"></i>{{item.name}}
+            </span>
+        </el-row>
+
+        <el-form class="search-form-content" ref="form" label-width="80px">
+            <el-form-item label="出发城市" prop="departCity">
+                <!-- fetch-suggestions 返回输入建议的方法,监听输入框的输入，可以在这个事件中请求API数据,类似input事件 -->
+                <!-- select 点击选中建议项时触发,点击展开列表选项时候触发 -->
+                <el-autocomplete
+                :fetch-suggestions="queryDepartSearch"
+                placeholder="请搜索出发城市"
+                @select="handleDepartSelect"
+                class="el-autocomplete"
+                v-model="form.departCity"
+                @blur="handleDepartCityBlur"
+                ></el-autocomplete>
+            </el-form-item>
+            <el-form-item label="到达城市" prop="destCity">
+                <el-autocomplete
+                :fetch-suggestions="queryDestSearch"
+                placeholder="请搜索到达城市"
+                @select="handleDestSelect"
+                class="el-autocomplete"
+                v-model="form.destCity"
+                @blur="handleDestcityBlur"
+                ></el-autocomplete>
+            </el-form-item>
+            <el-form-item label="出发时间">
+                <!-- change 用户确认选择日期时触发 -->
+                <el-date-picker type="date" 
+                placeholder="请选择日期" 
+                style="width: 100%;"
+                @change="handleDate"
+                v-model="form.departDate"
+                value-format="yyyy-MM-dd">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item label="">
+                <el-button style="width:100%;" 
+                type="primary" 
+                icon="el-icon-search"
+                @click="handleSubmit"
+               >
+                    搜索
+                </el-button>
+            </el-form-item>
+            <div class="reverse">
+                <span @click="handleReverse">换</span>
+            </div>
+        </el-form>  
+      </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            tabs: [
+                {icon: "iconfont icondancheng", name: "单程"},
+                {icon: "iconfont iconshuangxiang", name: "往返"}
+            ],
+            currentTab: 0,
+            // 城市机票
+            form: {
+                departCity: "",
+                departCode: "",
+                destCity: "",
+                destCode: "",
+                departDate: ""
+            },
+            // 模糊搜索数据合集
+            departCitys: [],
+            destCitys: []
+        }
+    },
+    methods: {
+        // tab切换时触发
+        handleSearchTab(item, index){
+            
+        },
+        
+        // 出发城市输入框获得焦点时触发
+        // value 是选中的值，cb是回调函数，接收要展示的列表
+        queryDepartSearch(value, cb){
+            if (!value) {
+                return;
+            }
+            // value为input框输入的值
+            // console.log(value)
+            this.$axios({
+                url: '/airs/city',
+                params: {
+                    name: value
+                }
+            }).then((res) => {
+                // console.log(res)
+                this.departCitys = res.data.data
+                this.departCitys = this.departCitys.map(item => {
+                    item.value = item.name.replace('市', '')
+                    return item
+                })
+                cb(this.departCitys);
+                // console.log(this.departCitys)
+            })     
+        },
+
+        // 目标城市输入框获得焦点时触发
+        // value 是选中的值，cb是回调函数，接收要展示的列表
+        queryDestSearch(value, cb){
+            if (!value) {
+                return;
+            }
+           this.$axios({
+                url: '/airs/city',
+                params: {
+                    name: value
+                }
+            }).then((res) => {
+                this.destCitys = res.data.data
+                this.destCitys = this.destCitys.map(item => {
+                    item.value = item.name.replace('市', '')
+                    return item
+                })
+                cb(this.destCitys);
+            })
+            // cb([
+            //     {value: 1},
+            //     {value: 2},
+            //     {value: 3},
+            // ]);
+        },
+       
+        // 出发城市下拉选择时触发
+        handleDepartSelect(item) {
+            // console.log(item)
+            this.form.departCity = item.value
+            this.form.departCode = item.sort
+        },
+
+        // 目标城市下拉选择时触发
+        handleDestSelect(item) {
+            this.form.destCity = item.value
+            this.form.destCode = item.sort
+        },
+
+        // 确认选择日期时触发
+        handleDate(value){
+            // 直接使用时间插件中的属性修改时间格式也行
+            //  this.form.departDate = moment(value).format("YYYY-MM-DD");
+        },
+
+        // 触发和目标城市切换时触发
+        handleReverse(){
+            
+        },
+
+        // 如果用户不选择则默认再失焦时选第一个数据
+        // 出发城市
+        handleDepartCityBlur () {
+            if (this.departCitys.length === 0) {
+                return;
+            }
+            this.form.departCity = this.departCitys[0].value
+            this.form.departCode = this.departCitys[0].sort
+        },
+
+        // 目标城市
+        handleDestcityBlur () {
+            if (this.destCitys.length === 0) {
+                return;
+            }
+            this.form.destCity = this.destCitys[0].value
+            this.form.destCode = this.destCitys[0].sort
+        },
+
+        // 提交表单是触发
+        handleSubmit(){
+           console.log(this.form)
+           this.$router.push({
+               path: "/air/flights",
+               query: this.form
+           })
+        }
+    },
+    mounted() {
+       
+    }
+}
+</script>
+
+<style scoped lang="less">
+.search-form{
+    border:1px #ddd solid;
+    border-top:none;
+    width:360px;
+    height:350px;
+    box-sizing: border-box;
+}
+
+.search-tab{
+  span{
+    display: block;
+    flex:1;
+    text-align: center;
+    height:48px;
+    line-height: 42px;
+    box-sizing: border-box;
+    border-top:3px #eee solid;
+    background:#eee;
+  }
+
+  .active{
+    border-top-color: orange;
+    background:#fff;
+  }
+
+  i{
+    margin-right:5px;
+    font-size: 18px;
+
+    &:first-child{
+      font-size:16px;
+    }
+  }
+}
+
+.search-form-content{
+  padding:15px 50px 15px 15px;
+  position: relative;
+
+  .el-autocomplete{
+    width: 100%;
+  }
+}
+
+.reverse{
+  position:absolute;
+  top: 35px;
+  right:15px;
+
+  &:after,&:before{
+      display: block;
+      content: "";
+      position: absolute;
+      left:-35px;
+      width:25px;
+      height:1px;
+      background:#ccc;
+  }
+
+  &:after{
+      top:0;
+    }
+
+    &:before{
+      top:60px;
+    }
+
+  span{
+    display: block;
+    position:absolute;
+    top: 20px;
+    right:0;
+    font-size:12px;
+    background: #999;
+    color:#fff;
+    width:20px;
+    height:20px;
+    line-height: 18px;
+    text-align: center;
+    border-radius: 2px;
+    cursor: pointer;
+
+    &:after,&:before{
+      display: block;
+      content: "";
+      position: absolute;
+      left:10px;
+      width:1px;
+      height:20px;
+      background:#ccc;
+    }
+
+    &:after{
+      top:-20px;
+    }
+
+    &:before{
+      top:20px;
+    }
+  }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'} // 新增全局字体样式
     ]
   },
   /*
@@ -31,7 +32,8 @@ export default {
   */
   plugins: [
     '@/plugins/element-ui',
-    { src: '@/plugins/localStorage', ssr: false }
+    { src: '@/plugins/localStorage', ssr: false },
+    '@/plugins/axios'
   ],
   /*
   ** Nuxt.js dev-modules
@@ -50,8 +52,8 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    // baseURL: "http://157.122.54.189:9095" // 新增备用地址(线上服务器地址)
-    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径 
+    baseURL: "http://157.122.54.189:9095" // 新增备用地址(线上服务器地址)
+    // baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径 
   },
   /*
   ** Build configuration
